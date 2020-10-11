456f4e9b3ac05885f04eba7c0ebe2ce3a6c1b4a4
Lazyeye79 <jdh346@nau.edu>
2020-10-11 09:31:33 -0400

    Cleaned up code

--- src/com/chris/display/Display.java
+++ src/com/chris/display/Display.java
@@ -4,6 +4,7 @@ import com.chris.element.Button;
 import com.chris.tree.Node;
 import com.chris.tree.Tree;
 import com.lazyeye79.engine.GameEngine;
+import com.lazyeye79.engine.draw.Rectangle;
 import com.lazyeye79.network.Message;
 import com.lazyeye79.network.client.Client;
 import com.lazyeye79.network.server.Server;
@@ -12,19 +13,28 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
 
 public class Display extends GameEngine {
 
-  private static final String NUM_REGEX = "[^0-9]+";
 
   public static final int SCREEN_WIDTH = 800;
   public static final int SCREEN_HEIGHT = 400;
+
+  private static final String NUM_REGEX = "[^0-9]+";
+  private static final String ADDRESS_REGEX = "[^a-zA-Z0-9.]+";
+
+  private static final String ENTER_PORT = "Enter Port";
+  private static final String ENTER_ADDRESS = "Enter Address";
+  private static final String CONNECTING = "Connecting";
+
+
 
   private static int radius = 25;
   private static float speed = 150;
 
   public enum State {
-    SETUP, RUNNING, HOST, JOIN, CONNECTING
+    SETUP, RUNNING, HOST, ADDRESS, PORT, JOIN, CONNECTING
   }
 
   public enum Actions {
@@ -35,6 +45,7 @@ public class Display extends GameEngine {
   private Server server;
   private Integer port;
   private String address;
+  private boolean portAlreadySet;
 
   // Buttons for navigation
   private Button offline;
@@ -51,6 +62,7 @@ public class Display extends GameEngine {
   private boolean center = true;
 
   private String typedValue;
+  private Rectangle typedValueBackground;
 
   private State state;
 
@@ -86,6 +98,8 @@ public class Display extends GameEngine {
     this.join = new Button(200, 25, 50, 20, Color.GRAY, "Join", Color.BLACK);
     this.next = new Button(SCREEN_WIDTH - 100, 25, 50, 20, Color.GRAY, "Next", Color.BLACK, false, Color.DARK_GRAY);
 
+    this.typedValueBackground = new Rectangle(8, 0, 100, 12, Color.GRAY, true);
+
     return true;
   }
 
@@ -97,220 +111,253 @@ public class Display extends GameEngine {
   @Override
   protected boolean onUserUpdate(float elapsedTime) {
 
-    MouseEvent mouseClicked = this.getMouseClickedEvent();
-
     // Blank the screen before redrawing
     this.blank(Color.WHITE);
 
-    if (this.state == State.RUNNING) {
+    MouseEvent mouseClicked = this.getMouseClickedEvent();
+    MouseEvent mouseReleased = this.getMouseReleasedEvent();
+    MouseEvent mouseDragged = this.getMouseDraggedEvent();
+    MouseWheelEvent mouseWheelEvent = this.getMouseWheelEvent();
 
-      if (this.client != null) {
-        while (this.client.isDataAvailable()) {
-          Message received = this.client.getReceivedData();
-          int payload = (int) received.getPayload();
-          if (received.getId() == Actions.ADD) {
-            this.tree.add(payload);
-          } else {
-            this.tree.remove(payload);
-          }
-        }
-      }
-
-      this.horizontalSpace = (int) (radius * 2 * Math.pow(2, this.tree.getLevels()) + (radius * 2 * 16));
-      // Clear the typed value if escaped is pressed
-      if (this.keyPressed(KeyEvent.VK_ESCAPE)) {
-        this.typedValue = "";
-      }
-      if (!this.typedValue.isEmpty()) {
-        if (this.keyReleased(KeyEvent.VK_E)) {
-          int val = Integer.parseInt(this.typedValue);
-          tree.add(val);
-          this.typedValue = "";
-
-          // Send data
-          if (this.client != null) {
-            Message<Actions> toSend = new Message<>(Actions.ADD, val);
-            this.client.send(toSend);
-          }
-
-        } else if (this.keyReleased(KeyEvent.VK_R)) {
-          int val = Integer.parseInt(this.typedValue);
-          tree.remove(val);
-          this.typedValue = "";
-
-          // Send data
-          if (this.client != null) {
-            Message<Actions> toSend = new Message<>(Actions.REMOVE, val);
-            this.client.send(toSend);
-          }
-        }
-      }
-
-      if (this.keyTyped()) {
-        char keyTyped = this.getKeyTyped();
-        if (keyTyped == 'z') {
-          this.scale += 0.1f;
-        }
-        if (keyTyped == 'x') {
-          this.scale -= 0.1f;
-        }
-        this.typedValue += keyTyped;
-        this.typedValue = this.typedValue.replaceAll(NUM_REGEX, "");
-      }
-
-      if (!this.typedValue.isEmpty()) {
-        this.drawText(this.typedValue, 10, 10, Color.BLACK);
-      }
-
-      // Move the camera
-      if (this.keyPressed(KeyEvent.VK_RIGHT)) {
-        this.cameraXOffset -= (speed * elapsedTime);
-      }
-      if (this.keyPressed(KeyEvent.VK_LEFT)) {
-        this.cameraXOffset += (speed * elapsedTime);
-      }
-      if (this.keyPressed(KeyEvent.VK_UP)) {
-        this.cameraYOffset += (speed * elapsedTime);
-      }
-      if (this.keyPressed(KeyEvent.VK_DOWN)) {
-        this.cameraYOffset -= (speed * elapsedTime);
-      }
-
-      MouseEvent mr = this.getMouseReleasedEvent();
-      MouseEvent md = this.getMouseDraggedEvent();
-      if (md != null) {
-        this.cameraXOffset = md.getX();
-        this.cameraYOffset = md.getY();
-      }
-      if (mr != null) {
-        this.resetMouseDraggedEvent();
-      }
-
-//    this.drawOval(100 - this.radius/2, 100 - this.radius/2, this.radius, this.radius, Color.BLACK);
-//    this.drawText(this.tree.getRoot().getValue() + "", 100, 100, Color.BLACK);
-
-      if (this.tree.getRoot() != null) {
-        depthFirstSearch(this.tree.getRoot(), 0, false, 0, 0, 0);
-      }
-    } else if (this.state == State.SETUP) {
-
-      this.draw(this.offline);
-      this.draw(this.host);
-      this.draw(this.join);
-
-      if (mouseClicked != null) {
-        int x = mouseClicked.getX();
-        int y = mouseClicked.getY();
-        if (this.offline.inBounds(x, y)) {
-          this.state = State.RUNNING;
-        }
-        if (this.host.inBounds(x, y)) {
-          this.state = State.HOST;
-        }
-        if (this.join.inBounds(x, y)) {
-          this.state = State.JOIN;
-        }
-      }
-
-      // Need to know if offline, hosting, joining
-      // If offline, enter RUNNING state
-      // If hosting, setup port for server, then join
-      // If joining, setup address and port for client
-
-    } else if (this.state == State.HOST) {
-
-      if (this.typedValue.isEmpty()) {
-        this.next.disable();
+    if (this.keyTyped()) {
+      char keyTyped = this.getKeyTyped();
+      if (keyTyped == KeyEvent.VK_BACK_SPACE) {
+        this.typedValue = this.typedValue.substring(0, this.typedValue.length()-1);
       } else {
-        this.next.enable();
+        this.typedValue += keyTyped;
       }
+    }
+    // Clear the typed value if escaped is pressed
+    if (this.keyPressed(KeyEvent.VK_ESCAPE)) {
+      this.typedValue = "";
+    }
 
-      this.draw(this.next);
-      if (!this.typedValue.isEmpty()) {
-        this.drawText(this.typedValue, 20, 50, Color.BLACK);
-      }
+    switch (this.state) {
 
-      if (this.keyTyped()) {
-        char typedKey = this.getKeyTyped();
-        this.typedValue += typedKey;
-        this.typedValue = this.typedValue.replaceAll(NUM_REGEX, "");
-      }
+      case RUNNING:
 
-      if (mouseClicked != null) {
-        if (this.next.inBounds(mouseClicked.getX(), mouseClicked.getY())) {
-          this.port = Integer.parseInt(this.typedValue);
-          this.typedValue = "";
-          if (this.server == null) {
-            this.server = new Server("Tree Server", this.port, true);
-            this.server.start();
+        if (this.client != null) {
+          while (this.client.isDataAvailable()) {
+            Message received = this.client.getReceivedData();
+            int payload = (int) received.getPayload();
+            if (received.getId() == Actions.ADD) {
+              this.tree.add(payload);
+            } else {
+              this.tree.remove(payload);
+            }
+          }
+        }
+
+        // Recalculate horizontal space
+        this.horizontalSpace = (int) (radius * 2 * Math.pow(2, this.tree.getLevels()) + (radius * 2 * 16));
+
+        // Sanitize input
+        if (!this.typedValue.isEmpty()) {
+          this.typedValue = sanitize(this.typedValue, NUM_REGEX);
+        }
+
+        // Add or remove values
+        if (!this.typedValue.isEmpty()) {
+          if (this.keyReleased(KeyEvent.VK_E)) {
+            int val = Integer.parseInt(this.typedValue);
+            tree.add(val);
+            this.typedValue = "";
+
+            // Send data
+            if (this.client != null) {
+              Message<Actions> toSend = new Message<>(Actions.ADD, val);
+              this.client.send(toSend);
+            }
+
+          } else if (this.keyReleased(KeyEvent.VK_R)) {
+            int val = Integer.parseInt(this.typedValue);
+            tree.remove(val);
+            this.typedValue = "";
+
+            // Send data
+            if (this.client != null) {
+              Message<Actions> toSend = new Message<>(Actions.REMOVE, val);
+              this.client.send(toSend);
+            }
+          }
+        }
+
+        // Handle zooming
+        if (mouseWheelEvent != null) {
+          // Negate the wheel rotation, because up is negative, but we want that to zoom us in
+          this.scale += (float) -mouseWheelEvent.getWheelRotation() / 10;
+          if (this.scale < 0.1f) {
+            this.scale = 0.1f;
+          }
+        }
+
+        this.draw(this.typedValueBackground);
+        if (!this.typedValue.isEmpty()) {
+          this.drawText(this.typedValue, 10, 10, Color.BLACK);
+        }
+
+        // Move the camera
+        if (this.keyPressed(KeyEvent.VK_RIGHT)) {
+          this.cameraXOffset -= (speed * elapsedTime);
+        }
+        if (this.keyPressed(KeyEvent.VK_LEFT)) {
+          this.cameraXOffset += (speed * elapsedTime);
+        }
+        if (this.keyPressed(KeyEvent.VK_UP)) {
+          this.cameraYOffset += (speed * elapsedTime);
+        }
+        if (this.keyPressed(KeyEvent.VK_DOWN)) {
+          this.cameraYOffset -= (speed * elapsedTime);
+        }
+
+        // Allow dragging of the screen
+        // TODO: Fix this
+        if (mouseDragged != null) {
+          this.cameraXOffset = mouseDragged.getX();
+          this.cameraYOffset = mouseDragged.getY();
+        }
+        if (mouseReleased != null) {
+          this.resetMouseDraggedEvent();
+        }
+
+        if (this.tree.getRoot() != null) {
+          depthFirstSearch(this.tree.getRoot(), 0, false, 0, 0, 0);
+        }
+        break;
+
+      case SETUP:
+
+        this.draw(this.offline);
+        this.draw(this.host);
+        this.draw(this.join);
+
+        if (mouseClicked != null) {
+          int x = mouseClicked.getX();
+          int y = mouseClicked.getY();
+          if (this.offline.inBounds(x, y)) {
+            this.state = State.RUNNING;
+          }
+          if (this.host.inBounds(x, y)) {
+            this.state = State.HOST;
+          }
+          if (this.join.inBounds(x, y)) {
             this.state = State.JOIN;
           }
         }
-      }
 
-    } else if (this.state == State.JOIN) {
-      if (this.typedValue.isEmpty()) {
-        this.next.disable();
-      } else {
-        this.next.enable();
-      }
+        // Need to know if offline, hosting, joining
+        // If offline, enter RUNNING state
+        // If hosting, setup port for server, then join
+        // If joining, setup address and port for client
+        break;
+      case HOST:
 
-      this.draw(this.next);
-      if (!this.typedValue.isEmpty()) {
-        this.drawText(this.typedValue, 20, 50, Color.BLACK);
-      }
+        this.drawText(ENTER_PORT, 20, 20, Color.BLACK);
 
-      if (this.keyTyped()) {
-        char typedKey = this.getKeyTyped();
-        if (typedKey == KeyEvent.VK_BACK_SPACE) {
-          if (!this.typedValue.isEmpty()) {
-            this.typedValue = this.typedValue.substring(0, this.typedValue.length() - 1);
-          }
+        this.typedValue = sanitize(this.typedValue, NUM_REGEX);
+        if (this.typedValue.isEmpty()) {
+          this.next.disable();
         } else {
-          this.typedValue += typedKey;
+          this.next.enable();
         }
-      }
 
-      if (mouseClicked != null) {
-        if (this.next.inBounds(mouseClicked.getX(), mouseClicked.getY())) {
-          if (this.address == null) {
+        this.draw(this.next);
+        if (!this.typedValue.isEmpty()) {
+          this.drawText(this.typedValue, 20, 50, Color.BLACK);
+        }
+
+        if (mouseClicked != null) {
+          if (this.next.inBounds(mouseClicked.getX(), mouseClicked.getY())) {
+            this.port = Integer.parseInt(this.typedValue);
+            this.portAlreadySet = true;
+            this.typedValue = "";
+            if (this.server == null) {
+              this.server = new Server("Tree Server", this.port, true);
+              this.server.start();
+            }
+            this.state = State.ADDRESS;
+          }
+        }
+        break;
+      case ADDRESS:
+
+        this.typedValue = sanitize(this.typedValue, ADDRESS_REGEX);
+        if (this.typedValue.isEmpty()) {
+          this.next.disable();
+        } else {
+          this.next.enable();
+        }
+
+        this.draw(this.next);
+        if (!this.typedValue.isEmpty()) {
+          this.drawText(this.typedValue, 20, 50, Color.BLACK);
+        }
+
+        if (mouseClicked != null) {
+          if (this.next.inBounds(mouseClicked.getX(), mouseClicked.getY())) {
             this.address = this.typedValue;
             this.typedValue = "";
-          } else if (this.port == null) {
-            this.typedValue = this.typedValue.replaceAll(NUM_REGEX, "");
-            if (!this.typedValue.isEmpty()) {
-              this.port = Integer.parseInt(this.typedValue);
-              this.typedValue = "";
-            }
-          } else {
-            this.state = State.CONNECTING;
-            this.typedValue = "";
+            this.state = State.PORT;
           }
         }
-      }
+        break;
+      case PORT:
+        if (this.portAlreadySet) {
+          this.state = State.JOIN;
+          break;
+        }
 
-    } else if (this.state == State.CONNECTING) {
-      if (this.address == null || this.port == null) {
-        this.state = State.SETUP;
-      } else {
-        this.client = new Client("Tree Client", this.address, this.port);
-        this.client.start();
+        this.typedValue = sanitize(this.typedValue, NUM_REGEX);
+        if (this.typedValue.isEmpty()) {
+          this.next.disable();
+        } else {
+          this.next.enable();
+        }
 
-        this.state = State.RUNNING;
-      }
+        this.draw(this.next);
+        if (!this.typedValue.isEmpty()) {
+          this.drawText(this.typedValue, 20, 50, Color.BLACK);
+        }
+
+        if (mouseClicked != null) {
+          if (this.next.inBounds(mouseClicked.getX(), mouseClicked.getY())) {
+            this.port = Integer.parseInt(this.typedValue);
+            this.typedValue = "";
+            this.state = State.JOIN;
+          }
+        }
+        break;
+      case JOIN:
+        if (this.address == null || this.port == null) {
+          this.state = State.SETUP;
+        } else if (this.client == null) {
+          this.client = new Client("Tree Client", this.address, this.port);
+          this.client.start();
+        }
+        this.state = State.CONNECTING;
+        break;
+      case CONNECTING:
+        if (client.isReady()) {
+          this.state = State.RUNNING;
+        }
+        if (this.client.getException() != null) {
+          this.state = State.SETUP;
+        }
     }
 
+    // Go to the setup page if there was an error in the client
+    // TODO: Check what the error was before abandoning it
     if (this.client != null) {
       if (this.client.getException() != null) {
-        this.client.getException().printStackTrace();
         this.state = State.SETUP;
+        this.client.stop();
+        this.client = null;
       }
     }
 
     return true;
   }
 
-  // Pass -1 for root node side
   private void depthFirstSearch(Node start, int level, boolean youAreRightChild, int parentSpace, int px, int py) {
 
     if (this.debug) {
@@ -371,6 +418,12 @@ public class Display extends GameEngine {
     this.drawText(node.getValue() + "", (int) ((x + radius) * scale + this.cameraXOffset), (int) ((y + radius) * scale + this.cameraYOffset), Color.BLACK, font);
   }
 
+
+
+
+  public static String sanitize(String toSanitize, String regex) {
+    return toSanitize.replaceAll(regex, "");
+  }
 
   public static void main(String[] args) {
     Display d = new Display(SCREEN_WIDTH, SCREEN_HEIGHT, "Tree");
